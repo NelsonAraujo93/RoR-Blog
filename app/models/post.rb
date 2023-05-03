d92bebf059b62d905bf8399f288e31577eb19d70
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-03 16:22:08 -0500

    Add validations to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   after_save :update_post_counter
 
+  validates :title, presence: true, length: { maximun: 250 }
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_five_comments
     Comment.where(post_id: id).last(5)
   end

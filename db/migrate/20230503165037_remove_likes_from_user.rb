fe11559e2053eaf1a1f8f3f6922d2e5e2fb7c799
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-09 14:55:54 -0500

    Remove migration files with extra steps

--- db/migrate/20230503165037_remove_likes_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveLikesFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :likes, :users, null: false, foreign_key: true
-    add_reference :likes, :user, null: false, foreign_key: true
-  end
-end

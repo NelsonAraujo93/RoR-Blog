76c2f14484e5b6c75ae554b6c4260b64abd55bd8
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-08 20:12:09 -0500

    Fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,42 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it "renders index template" do
+    it 'renders index template' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
 
-    it "renders a successful response" do
+    it 'renders a successful response' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}/posts"
       expect(response).to be_successful
     end
 
-    it "Test if users/:user_id/posts is loading correctly the body" do
+    it 'Test if users/:user_id/posts is loading correctly the body' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}/posts"
-      expect(response.body).to include("Here is a list of posts for a given user")
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
   describe 'GET /show' do
-    let(:user) { User.create(name: "John", posts_counter: 0) }
+    let(:user) { User.create(name: 'John', posts_counter: 0) }
     let(:post) { Post.create(author: user, title: 'This is a title', comments_counter: 0, likes_counter: 0) }
 
-    it "renders show template" do
+    it 'renders show template' do
       get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template(:show)
     end
 
-    it "renders a successful response" do
+    it 'renders a successful response' do
       get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to be_successful
     end
 
-    it "Test if /users/:user_id/posts/:id is loading correctly the body" do
+    it 'Test if /users/:user_id/posts/:id is loading correctly the body' do
       get "/users/#{user.id}/posts/#{post.id}"
-      expect(response.body).to include("This is the detail view from a given post from the given user")
+      expect(response.body).to include('This is the detail view from a given post from the given user')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,41 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  
   describe 'GET /index' do
-    it "renders index template" do
+    it 'renders index template' do
       get '/users'
       expect(response).to render_template(:index)
     end
 
-    it "renders a successful response" do
-      get "/users"
+    it 'renders a successful response' do
+      get '/users'
       expect(response).to be_successful
     end
 
-    it "Test if /users is loading correctly the body" do
+    it 'Test if /users is loading correctly the body' do
       get '/users'
-      expect(response.body).to include("This is a list of users")
+      expect(response.body).to include('This is a list of users')
     end
   end
 
   describe 'GET /show' do
-    it "renders show template" do
+    it 'renders show template' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}"
       expect(response).to render_template(:show)
     end
 
-    it "renders a successful response" do
+    it 'renders a successful response' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}"
       expect(response).to be_successful
     end
 
-    it "Test if /users/:id is loading correctly the body" do
+    it 'Test if /users/:id is loading correctly the body' do
       user = User.create(name: 'Nelson', posts_counter: 0)
       get "/users/#{user.id}"
-      expect(response.body).to include("Here is detail view from a given user")
+      expect(response.body).to include('Here is detail view from a given user')
     end
   end
 end

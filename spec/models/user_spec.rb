6fb7b84b4c668026df504ef4013ade0250c93d90
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-15 16:04:22 -0500

    add testings with capybara for user#index

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  subject { User.new(name: 'Nelson', posts_counter: 0) }
+RSpec.describe User, type: :system do
+  subject { User.new(name: 'Nelson', posts_counter: 2, photo:'https://picsum.photos/300/300') }
+  let(:kender) {User.new(name: 'Kender', posts_counter: 0, photo:'https://picsum.photos/300/300')}
   before { subject.save }
+  before { kender.save }
+  describe 'index page' do
+    it 'shows the rendering of user Kender name' do
+      visit '/'
+      sleep(2)
+      expect(page).to have_content('Kender')
+    end
+    it 'shows the rendering of user Nelson name' do
+      visit '/'
+      sleep(2)
+      expect(page).to have_content('Nelson')
+    end
+    it 'show the images from user Kender' do
+      visit '/'
+      sleep(4)
+      expect(page.find("#user-#{kender.id}")['src']).to have_content 'https://picsum.photos/300/300'
+    end
+    it 'show the images from user Nelson' do
+      visit '/'
+      sleep(4)
+      expect(page.find("#user-#{subject.id}")['src']).to have_content 'https://picsum.photos/300/300'
+    end
+    it 'show the posts_counter from user Kender' do
+      visit '/'
+      sleep(2)
+      expect(page.find("#counter-#{kender.id}")).to have_content kender.posts_counter.to_s
+    end
+    it 'show the posts_counter from user Nelson' do
+      visit '/'
+      sleep(2)
+      expect(page.find("#counter-#{subject.id}")).to have_content subject.posts_counter.to_s
+    end
+    it 'redirects from user index to user show Nelson' do
+      visit '/' # Visit the user index page
+      sleep(2)
+      find('span', text: subject.name).click # Click on the user's link
+      expect(page).to have_current_path("/users/#{subject.id}", ignore_query: true) # Verify the redirection to user show page
+    end
+    it 'redirects from user index to user show Kender' do
+      visit '/' # Visit the user index page
+      sleep(2)
+      find('span', text: kender.name).click # Click on the user's link
+      expect(page).to have_current_path("/users/#{kender.id}", ignore_query: true) # Verify the redirection to user show page
+    end
+  end
+
   it 'name should be present, expected false' do
     subject.name = nil
     expect(subject).to_not be_valid

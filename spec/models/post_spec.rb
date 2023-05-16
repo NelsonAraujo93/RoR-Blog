58b83b1b514366305a3c7d44e13cb8e2b67e80e3
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-05-16 09:39:06 -0500

    Add all testings for posts index

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,77 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  let(:user) { User.new(name: 'Nelson', posts_counter: 0) }
-  subject { Post.create(author: user, title: 'This is a title', likes_counter: 0, comments_counter: 0) }
+RSpec.describe Post, type: :system do
+  let(:user) { User.new(name: 'Nelson', posts_counter: 0, photo:'https://picsum.photos/300/300',bio:'Nelson bio') }
+  subject { Post.create(author: user, title: 'This is a title', text:'And this is the post body', likes_counter: 0, comments_counter: 0) }
+  let(:kender) {User.new(name: 'Kender', posts_counter: 0, photo:'https://picsum.photos/300/300',bio:'Kender bio')}
+  let(:post_1) { Post.new(author: user, title: 'This is a title 2', likes_counter: 0, comments_counter: 0) }
+  let(:post_2) { Post.new(author: user, title: 'This is a title 3', likes_counter: 0, comments_counter: 0) }
+  let(:post_3) { Post.new(author: user, title: 'This is a title 4', likes_counter: 0, comments_counter: 0) }
+  let(:comment) { Comment.new(text: 'This is the comment #0', post: subject, author: user)}
 
   before { subject.save }
+  before { kender.save }
+  before { user.save }
+  before {post_1.save}
+  before {post_2.save}
+  before {post_3.save}
+  before {comment.save}
+
+  # INDEX PAGE CAPYBARA
+  describe 'index page' do
+    it 'show the user profile picture' do
+      visit "/users/#{user.id}/posts"
+      sleep(4)
+      expect(page.find("img")['src']).to have_content user.photo
+    end
+    it 'shows the User name' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page).to have_content(user.name)
+    end
+    it 'show the posts_counter from user profile' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page.find(".custom-card-t")).to have_content user.posts_counter.to_s
+    end
+    it 'Show the post title on posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page).to have_content subject.title
+    end
+    it 'Show the post body on posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page).to have_content subject.text
+    end
+    it 'Show the first comment of a post on posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page).to have_content comment.text
+    end
+    it 'Show the number of comments of a post on posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page.find("#comments-#{subject.id}")).to have_content subject.comments_counter.to_s
+    end
+    it 'Show the number of likes of a post on posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page.find("#likes-#{subject.id}")).to have_content subject.likes_counter.to_s
+    end
+    it 'Show the see all post button of a posts#index' do
+      visit "/users/#{user.id}/posts"
+      sleep(2)
+      expect(page).to have_content('Pagination')
+    end
+    it 'redirects from post#index to post#show' do
+      visit "/users/#{user.id}/posts" # Visit the user index page
+      sleep(2)
+      find("#test-#{subject.id}", text: subject.title).click # Click on the user's link
+      expect(page).to have_current_path("/users/#{user.id}/posts/#{subject.id}", ignore_query: true) # Verify the redirection to user show page
+    end
+  end
+
 
   it 'title should be present, expected false' do
     subject.title = nil
